41992d8bfc42477ff6e0d2cdf5f14c9a0b093d52
beyonderzbaby <vafffi476@gmail.com>
2026-09-13 16:46:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/service-worker.js")
+    navigator.serviceWorker.register("/Instagram.pwb/service-worker.js")
       .then(registration => console.log("Service worker registered:", registration.scope))
       .catch(error => console.error("Service worker registration failed:", error));
   });
