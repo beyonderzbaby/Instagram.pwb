e547786da68671298a8a5343c269cf5dcb0f8bc2
beyonderzbaby <vafffi476@gmail.com>
2026-09-13 16:23:33 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/service-worker.js")
+      .then(registration => console.log("Service worker registered:", registration.scope))
+      .catch(error => console.error("Service worker registration failed:", error));
+  });
+}
